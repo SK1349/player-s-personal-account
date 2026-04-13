44142f192dff67f4452fd1db0cc5bdcf45a06556
SK1349 <boo.ochevidno@gmail.com>
2026-04-13 23:36:59 +0300

    spring

---
+++ src/main/java/com/example/player_s_personal_account/PlayerSPersonalAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.player_s_personal_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerSPersonalAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerSPersonalAccountApplication.class, args);
+	}
+
+}
